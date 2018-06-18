0b5827039f8ed2c8416743f99def6c61f7f2a74c
rafagarciac <r.gc@hotmail.es>
2018-06-19 01:26:38 +0200

    Add Static Files by Default and Add new Template index.html. The main of web Page!! :)

--- main/views.py
+++ main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+'''
 def index (request):
     return HttpResponse("<h1> Hello World! </h1>")
+'''
+def index (request):
+    return render(request, 'main/index.html', context=None, content_type=None, status=None, using=None)
     
